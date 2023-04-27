8f1c878bf44c6edee725b4b05bdfd5c156d6a3e8
ON22LS <118619174+ON22LS@users.noreply.github.com>
2023-04-27 19:15:36 +0200

    Fehler bei falscher Angabe ausgeben

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
-console.log("helloWorld");
+function applyPuzzleEffect (container: HTMLElement, options: {columns: number, rows: number, }) { /*deklarieren was es für ein Element ist, damit nicht any*/
+
+if (!container) {
+    return console.error("No container was found");
+}
+const img = container.querySelector("img");
+
+if (!img) {
+    return console.error("Could not find image");
+}
+}
+
+
+const imageContainer = document.getElementById("ImageContainer");
+
+applyPuzzleEffect(imageContainer, {columns: 6, rows: 14 });
