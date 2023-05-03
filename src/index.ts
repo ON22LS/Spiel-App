d85d5be7495d6e634a459f5c67d274082aa875fa
ON22LS <118619174+ON22LS@users.noreply.github.com>
2023-05-03 09:56:30 +0200

    fix problems

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
+import {applyPuzzleEffect} from './src/puzzle';
--- src/puzzle.ts
+++ src/puzzle.ts
@@ -1,25 +1,32 @@
-function applyPuzzleEffect (container: HTMLElement, options: {columns: number, rows: number, }) /*spezifizieren was es für ein Element ist, damit nicht any*/
-{
+export function applyPuzzleEffect (container: HTMLElement, options: {
+  columns: number;
+  rows: number;
+  spread: number;
+}) {
   if (!container) { /*wenn kein Container übergeben worden ist*/
     return console.error("No container was found");
   }
 
   const img = container.querySelector("img"); /*sucht nach Image Elementen*/
-
   if (!img) { /*wenn kein Image gefunden worden ist*/
     return console.error("Could not find image");
   }  
 
-  const {columns, rows} = options; /*distructering: columns und rows ausschließen aus options*/
+  const {columns, rows, spread} = options; /*distructering: columns und rows ausschließen aus options*/
 
-  if (columns <= 0 || rows <= 0 || columns != Math.floor(columns) || rows != Math.floor(rows)) {/*testen, ob Zahlen positiv sind oder keine ganze Zahl*/
+  if (
+    columns <= 0 || 
+    rows <= 0 || 
+    columns != Math.floor(columns) || 
+    rows != Math.floor(rows)
+    ) {     /*testen, ob Zahlen positiv sind oder keine ganze Zahl*/
     return console. error("Only positive integers are allowed");
   }
 
-    img.style.opacity = "0"; /*Bild anfangs ausblenden (CSS Werte müssen immer String sein, daher "0")*/
+     /*Bild anfangs ausblenden (CSS Werte müssen immer String sein, daher "0")*/
     container.style.position = "relative";  /*Puzzlestücke absolut im Container platzieren, damit sie dem Bild nich in die Quere kommen, müssen sich überlagern können*/
 
-    const generatePieces = () => {/*Puzzlestücke erzeugen*/
+  const generatePieces = () => {/*Puzzlestücke erzeugen*/
        for (let y = 0; y < rows; y++) { /*Vorschleife erzeugen, y-Were durchgehen*/
          for (let x = 0; x < columns; x++) { /*x-Werte durchgehen, wo soll das Puzzlestück hin?*/
     
@@ -27,20 +34,24 @@ function applyPuzzleEffect (container: HTMLElement, options: {columns: number, r
          const correctY = (y/ rows) * img.height;
     /*Endposition ist am Anfang schon zu sehen*/
     
-    const piece = document.createElement("div");   /*Puzzlestück erstellen*/
+    const piece = document.createElement("div");  /*Puzzlestück erstellen*/
     piece.classList.add("piece");  /*Klasse erstellen, damit Effekt am Ende nicht mehr so schwer zu entfernen ist*/
 
     piece.style.position = "absolute";
     piece.style.top = piece.style.left = "0px";/*soll oben links angezeigt werden*/
     piece.style.width = `${img.width / columns}px`; /*richtet sich nach der Breite des Bildes und der Anzahl der Spalten*/
-    piece.style.height = `${img.height / rows}px`; 
+    piece.style.height = `${img.height / rows}px`;
+    piece.style.transform = `translate(${correctX}px,${correctY}px)`;   /*Puzzleteile sollen nicht mehr alle auf einem Punkt sein (template String)*/
     piece.style.outline = "4px solid red";
-    piece.style.transform = `translateX(${correctX}px,${correctY})px`;   /*Puzzleteile sollen nicht mehr alle auf einem Punkt sein (template String)*/
 
-    container.appendChild(piece);
+    container.appendChild(piece); 
+   
 }
 }
 }
+const imageContainer = document.getElementById("imageContainer") as HTMLElement;
+
+applyPuzzleEffect(imageContainer, { columns:6, rows:14, spread: 100});
 
 img.onload = () => {  /*Eventlistener*/
 generatePieces();
@@ -48,8 +59,3 @@ generatePieces();
 
 
 }
-
-
-const imageContainer = document.getElementById("ImageContainer") as HTMLElement;
-
-applyPuzzleEffect(imageContainer, {columns: 6, rows: 14 });
