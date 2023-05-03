4acb23a2a80b17d8d47db5947f6cca549405efc3
ON22LS <118619174+ON22LS@users.noreply.github.com>
2023-05-03 10:16:01 +0200

    export function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
-import {applyPuzzleEffect} from './src/puzzle';
+import {applyPuzzleEffect} from './puzzle';
+
+const createEffect = applyPuzzleEffect;
--- src/puzzle.ts
+++ src/puzzle.ts
@@ -1,61 +1,68 @@
-export function applyPuzzleEffect (container: HTMLElement, options: {
-  columns: number;
-  rows: number;
-  spread: number;
+console.log("hi mom");
+
+export function applyPuzzleEffect(container: HTMLElement, options: {
+    columns: number;
+    rows: number;
+    spread: number;
 }) {
-  if (!container) { /*wenn kein Container übergeben worden ist*/
-    return console.error("No container was found");
-  }
-
-  const img = container.querySelector("img"); /*sucht nach Image Elementen*/
-  if (!img) { /*wenn kein Image gefunden worden ist*/
-    return console.error("Could not find image");
-  }  
-
-  const {columns, rows, spread} = options; /*distructering: columns und rows ausschließen aus options*/
-
-  if (
-    columns <= 0 || 
-    rows <= 0 || 
-    columns != Math.floor(columns) || 
-    rows != Math.floor(rows)
-    ) {     /*testen, ob Zahlen positiv sind oder keine ganze Zahl*/
-    return console. error("Only positive integers are allowed");
-  }
-
-     /*Bild anfangs ausblenden (CSS Werte müssen immer String sein, daher "0")*/
-    container.style.position = "relative";  /*Puzzlestücke absolut im Container platzieren, damit sie dem Bild nich in die Quere kommen, müssen sich überlagern können*/
-
-  const generatePieces = () => {/*Puzzlestücke erzeugen*/
-       for (let y = 0; y < rows; y++) { /*Vorschleife erzeugen, y-Were durchgehen*/
-         for (let x = 0; x < columns; x++) { /*x-Werte durchgehen, wo soll das Puzzlestück hin?*/
+    if (!container) { /*wenn kein Container übergeben worden ist*/
+      return console.error("No container was found");
+    }
+  
+    const img = container.querySelector("img"); /*sucht nach Image Elementen*/
+    if (!img) { /*wenn kein Image gefunden worden ist*/
+      return console.error("Could not find image");
+    }  
+  
+    const {columns, rows, spread} = options; /*distructering: columns und rows ausschließen aus options*/
+  
+    if (
+      columns <= 0 || 
+      rows <= 0 || 
+      columns != Math.floor(columns) || 
+      rows != Math.floor(rows)
+      ) {     /*testen, ob Zahlen positiv sind oder keine ganze Zahl*/
+      return console. error("Only positive integers are allowed");
+    }
     
-         const correctX = (x / columns ) * img.width;/*richtige x-Position, wo es am Ende landet (x durch Anzahl der Spalten) mal die Breite des Bildes*/
-         const correctY = (y/ rows) * img.height;
-    /*Endposition ist am Anfang schon zu sehen*/
-    
-    const piece = document.createElement("div");  /*Puzzlestück erstellen*/
-    piece.classList.add("piece");  /*Klasse erstellen, damit Effekt am Ende nicht mehr so schwer zu entfernen ist*/
+    img.style.opacity = "0";
+    container.style.position = "relative";
 
-    piece.style.position = "absolute";
-    piece.style.top = piece.style.left = "0px";/*soll oben links angezeigt werden*/
-    piece.style.width = `${img.width / columns}px`; /*richtet sich nach der Breite des Bildes und der Anzahl der Spalten*/
-    piece.style.height = `${img.height / rows}px`;
-    piece.style.transform = `translate(${correctX}px,${correctY}px)`;   /*Puzzleteile sollen nicht mehr alle auf einem Punkt sein (template String)*/
-    piece.style.outline = "4px solid red";
+    const generatePieces = () => {
+        for (let y = 0; y < rows; y++) { 
+           for (let x = 0; x < columns; x++) { 
+      
+           const correctX = (x / columns ) * img.width;
+           const correctY = (y/ rows) * img.height;
 
-    container.appendChild(piece); 
-   
+           const randomX = correctX + (2*Math.random() -1) * spread;
+           const randomY = correctY + (2*Math.random() -1) * spread;
+
+      const piece = document.createElement("div");
+      piece.classList.add("piece");
+
+      piece.style.position = "absolute";
+      piece.style.top = piece.style.left = "0px";
+      piece.style.width = `${img.width / columns}px`; 
+      piece.style.height = `${img.height / rows}px`;
+      piece.style.transform = `translate(${randomX}px,${randomY}px)`;
+      piece.style.outline = "1px solid red";
+
+      piece.style.backgroundImage = `url(${img.src})`;
+      piece.style.backgroundPositionX = `-${correctX}px`;
+      piece.style.backgroundPositionY = `-${correctY}px`;
+
+      container.appendChild(piece);
+      
+    }
 }
+    }
+
+    img.onload = () => {
+        generatePieces();
+    }
 }
-}
+
 const imageContainer = document.getElementById("imageContainer") as HTMLElement;
 
 applyPuzzleEffect(imageContainer, { columns:6, rows:14, spread: 100});
-
-img.onload = () => {  /*Eventlistener*/
-generatePieces();
-}
-
-
-}
